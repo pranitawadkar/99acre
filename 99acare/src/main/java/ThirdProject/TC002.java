3f09157939fd1378e54fe929d814a2ad267e1924
Admin <Admin@DESKTOP-S7KHIBK>
2023-02-16 23:05:22 +0530

    bhavana

--- 99acare/src/main/java/ThirdProject/TC002.java
+++ 99acare/src/main/java/ThirdProject/TC002.java
@@ -4,6 +4,9 @@ public class TC002 {
 	public static void main(String args[])
 	{
 		System.out.println("hi");
+		System.out.println("Bye");
+		System.out.println("Bye");
+		System.out.println("Bye");
 	}
 
 }
